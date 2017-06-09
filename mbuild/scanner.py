7a97205bc503198fbf02091f484cf4d758cef00d
Mark Charney <mark.charney@intel.com>
2017-06-09 19:40:46 -0400

    scanner: file open idiom
    Change-Id: Ic7e574227de72fd8068acb8af83b345b5b2dbf97
    (cherry picked from commit 3ee19239e31f468eb380303885abef671baaac3e)

--- mbuild/scanner.py
+++ mbuild/scanner.py
@@ -57,6 +57,13 @@ def mbuild_compute_path(hname, search_path):
 mbuild_include_pattern = re.compile(r'^[ \t]*#[ \t]*include[ \t]+"(?P<hdr>[^"]+)"')
 mbuild_nasm_include_pattern = re.compile(r'^[ \t]*%include[ \t]+"(?P<hdr>[^"]+)"')
 
+is_py2 = sys.version[0] == '2'
+def _open_errors(fn):
+    if is_py2:
+        return open(fn, 'r')
+    else:
+        return open(fn, 'r', errors='ignore')
+    
 def mbuild_scan(fn, search_path):
     """Given a file name fn, and a list of search paths, scan for
     headers in fn and return a list of mbuild_header_record_t's. The
@@ -75,21 +82,22 @@ def mbuild_scan(fn, search_path):
     if source_path == '':
         source_path = '.'
     aug_search_path = [source_path] + search_path
-        
-    for line in file(fn).readlines():
-        #print (line)
-        hgroup = mbuild_include_pattern.match(line)
-        if not hgroup:
-            hgroup = mbuild_nasm_include_pattern.match(line)
-        if hgroup:
-            hname =  hgroup.group('hdr')
-            #print (hname)
-            full_name = mbuild_compute_path(hname, aug_search_path)
-            if full_name:
-                hr = mbuild_header_record_t(full_name)
-            else:
-                hr = mbuild_header_record_t(hname, found=False)
-            all_names.append(hr)
+
+    with _open_errors(fn) as f:
+        for line in f:
+            #print (line)
+            hgroup = mbuild_include_pattern.match(line)
+            if not hgroup:
+                hgroup = mbuild_nasm_include_pattern.match(line)
+            if hgroup:
+                hname =  hgroup.group('hdr')
+                #print (hname)
+                full_name = mbuild_compute_path(hname, aug_search_path)
+                if full_name:
+                    hr = mbuild_header_record_t(full_name)
+                else:
+                    hr = mbuild_header_record_t(hname, found=False)
+                all_names.append(hr)
     return all_names
 
 
